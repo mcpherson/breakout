bfc4a640a4ea743e4b3438eec692968371381214
Marlon <583056+mcpherson@users.noreply.github.com>
2024-07-11 00:26:47 -0600

    Added score multiplier for faster breaks

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -8,15 +8,22 @@ using TMPro;
 
 public class Ball : MonoBehaviour
 {
+    // BALL
     Rigidbody2D _rb;
     float _minY = -6.5f;
     [SerializeField] private float _speed = 200.0f;
     Vector2 _direction;
     float _maxVelocity = 8f;
     float _minVelocity = 4f;
-    int score = 0;
-    int lives = 5;
-    [SerializeField] TextMeshProUGUI scoreText;
+
+    // HUD
+    float _score = 0;
+    float _baseScore = 100;
+    int _lives = 5;
+    [SerializeField] TextMeshProUGUI _scoreText;
+    float _timeSinceLastBreak = 0f;
+    float _maxTimeSinceLastBreak = 2f;
+
 
     void AddStartingForce()
     {
@@ -36,22 +43,25 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // update timer
+        _timeSinceLastBreak += Time.deltaTime;
+
         // min/max ball speed
         if (_rb.velocity.magnitude > _maxVelocity)
         {
-            Debug.Log("too fast");
+            // Debug.Log("too fast");
             _rb.velocity = Vector3.ClampMagnitude(_rb.velocity, _maxVelocity);
         } 
         else if (_rb.velocity.magnitude < _minVelocity && _rb.velocity.magnitude > 0.1) // 
         {
-            Debug.Log("too slow");
+            // Debug.Log("too slow");
             _rb.velocity = Vector3.ClampMagnitude(_rb.velocity.normalized * 5f, _maxVelocity);
         }
 
         // handle out-of-bounds
         if (transform.position.y < _minY)
         {
-            lives--;
+            _lives--;
             transform.position = Vector3.zero;
             // nudge the ball downward to prevent soft-lock conditions
             _rb.velocity = new Vector2(0.0f, -0.01f);
@@ -64,17 +74,33 @@ public class Ball : MonoBehaviour
             Debug.Log("Soft-lock prevented!");
             _rb.AddForce(_direction * 1f);
         }
+
     }
 
     void OnCollisionEnter2D(Collision2D collision) 
     {
         if (collision.gameObject.CompareTag("Brick")) 
         {
-            score += 100;
-            scoreText.text = score.ToString();
+            // scoring
+            if (_timeSinceLastBreak >= _maxTimeSinceLastBreak) // no multiplier
+            {
+                _score += _baseScore;
+            }
+            else // multiplier increases as time since last break decreases
+            {
+                float _scoreToAdd = _baseScore * ((_maxTimeSinceLastBreak - _timeSinceLastBreak) + 1);
+                Debug.Log($"add: {_scoreToAdd}, time since last: {_timeSinceLastBreak}");
+                _score += _scoreToAdd;
+            } 
+            
+
+            _timeSinceLastBreak = 0;
+            _scoreText.text = Mathf.Round(_score).ToString();
             Destroy(collision.gameObject);
         }
     }
 
     
 }
+
+// TODO: new game/pause menus, score flashes @ brick position when brick breaks, game modes, sliders, control mapping
